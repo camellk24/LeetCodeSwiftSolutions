79c569bd6a775954b7867062988d983a83b4d313
Ke Luo <camellk24@gmail.com>
2018-07-15 22:00:24 -0600

    Adds binary search solution to 300. Longest Increasing Subsequence

--- Solutions/300.LongestIncreasingSubsequence.playground/Contents.swift
+++ Solutions/300.LongestIncreasingSubsequence.playground/Contents.swift
@@ -64,6 +64,67 @@ class Solution {
   }
 }
 
+class Solution_Binary_Search {
+  func lengthOfLIS(_ nums: [Int]) -> Int {
+    
+    let n = nums.count
+    
+    guard n > 0 else {
+      return 0
+    }
+    
+    //    10, 9, 2, 5, 3, 7, 101, 18
+    //  f  1  1  1  2
+    // looking at 10 -> 1, 9 -> 1, we will not use 10 -> 1 because they have same number of subsequence but 10 requires larger number, so we just need to remember 9
+    // similarly, when we have 9 -> 1, 2 -> 1, we know that we can ignore 9 and just remember 2 -> 1
+    // looking at 2 -> 1, 5 -> 2, we can tell that if we know find the largest number in previous numbers that small than current one, we can increment by 1
+    // we can use binary search
+    
+    // -inf, inf, ....
+    // init array with n + 1 size
+    // set first one to negative inf and the rest to inf
+    // then we can use binary search
+    var f: [Int] = Array(repeating: Int.max, count: n + 1)
+    f[0] = Int.min
+    
+    var maxCount = 0
+    
+    for i in 0 ..< n {
+      let num = nums[i]
+      let index = binarySearch(f, num)
+      f[index] = num
+      maxCount = max(index, maxCount)
+    }
+    
+    return maxCount
+  }
+  
+  
+  ///  Find the first number > target
+  ///
+  /// - Parameters:
+  ///   - nums:
+  ///   - target:
+  /// - Returns:
+  private func binarySearch(_ nums: [Int], _ target: Int) -> Int {
+    
+    var start = 0
+    var end = nums.count - 1
+    
+    while (start + 1 < end) {
+      let mid = (end - start) / 2 + start
+      if nums[mid] < target {
+        start = mid
+      } else {
+        end = mid
+      }
+    }
+    return end
+  }
+}
+  
+  
+
 /*:
  ## Test
  */
@@ -71,11 +132,35 @@ import XCTest
 
 class TestLongestIncreasingSubsequence: XCTestCase {
   
-  func testLongestIncreasingSubsequence() {
+  func testLongestIncreasingSubsequence1() {
+    
+    let input = [10,9,2,5,3,7,101,18]
+    
     let solution = Solution()
-    let result = solution.lengthOfLIS([10,9,2,5,3,7,101,18])
+    let result = solution.lengthOfLIS(input)
+    
+    let solution_binary_search = Solution_Binary_Search()
+    let result_binary_search = solution_binary_search.lengthOfLIS(input)
+    
     XCTAssertEqual(result, 4)
+    XCTAssertEqual(result_binary_search, 4)
   }
+  
+  func testLongestIncreasingSubsequence2() {
+    
+    let input = [1,3,6,7,9,4,10,5,6]
+    
+    let solution = Solution()
+    let result = solution.lengthOfLIS(input)
+    
+    let solution_binary_search = Solution_Binary_Search()
+    let result_binary_search = solution_binary_search.lengthOfLIS(input)
+    
+    XCTAssertEqual(result, 6)
+    XCTAssertEqual(result_binary_search, 6)
+  }
+  
+  
   
 }
 
