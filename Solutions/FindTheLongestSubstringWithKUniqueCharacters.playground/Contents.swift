4ecd743ff20205fd36642625af6317e374f985c1
Ke Luo <camellk24@gmail.com>
2018-07-26 14:03:52 -0600

    - update reference for Find the longest substring with k unique characters

--- Solutions/FindTheLongestSubstringWithKUniqueCharacters.playground/Contents.swift
+++ Solutions/FindTheLongestSubstringWithKUniqueCharacters.playground/Contents.swift
@@ -2,6 +2,8 @@
 /*:
  # Find the longest substring with k unique characters
  
+ References: https://www.geeksforgeeks.org/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
+ 
  Given a string you need to print longest possible substring that has exactly M unique characters. If there are more than one substring of longest possible length, then print any one of them.
  
  Examples:
