1550833df6ff53293fb8cfc24151ba01fad3fc08
Ke Luo <camellk24@gmail.com>
2018-08-11 18:29:02 -0600

    - update description

--- Solutions/034.FindFirstAndLastPositionofElementinSortedArray.playground/Contents.swift
+++ Solutions/034.FindFirstAndLastPositionofElementinSortedArray.playground/Contents.swift
@@ -1,24 +1,20 @@
 /*:
  # 34. Find First and Last Position of Element in Sorted Array
  
- Given a set of distinct integers, nums, return all possible subsets (the power set).
+ Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
  
- Note: The solution set must not contain duplicate subsets.
+ Your algorithm's runtime complexity must be in the order of O(log n).
  
- For example,
- If nums = [1,2,3], a solution is:
+ If the target is not found in the array, return [-1, -1].
  
- [
- [3],
- [1],
- [2],
- [1,2,3],
- [1,3],
- [2,3],
- [1,2],
- []
- ]
+ Example 1:
  
+ Input: nums = [5,7,7,8,8,10], target = 8
+ Output: [3,4]
+ Example 2:
+ 
+ Input: nums = [5,7,7,8,8,10], target = 6
+ Output: [-1,-1]
  */
 
 /*:
